679ecca28bce5dbcfc7eec75b4bb835cc9331dde
zeffanyasyahriel <zeffanyasyahriel244@outlook.com>
2020-04-26 12:12:08 +0700

    setting routes diserver js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app =express();
 //parser application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+//panggil routes
+var routes = require('./routes');
+routes(app);
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
